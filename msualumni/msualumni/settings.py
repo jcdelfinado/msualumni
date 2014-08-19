101ee47454e4b773483825042dfee0e097e776a1
jcdelfinado <jerico.delfinado@gmail.com>
2014-08-19 13:21:52 +0800

    removed django-simple-captcha

--- msualumni/msualumni/settings.py
+++ msualumni/msualumni/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.webdesign',
-    'captcha',
     'bootstrap3',
     'south',
     'profiles',
